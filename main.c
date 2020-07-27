0e0b47f3fccbbd27b7b04bbabbafd71018dc61df
openmind <pirogov27@yahoo.com>
2020-07-28 00:14:12 +0300

    change code style

--- main.c
+++ main.c
@@ -10,9 +10,7 @@
 #include <unistd.h>
 
 
-
-typedef struct 
-{
+typedef struct {
 	uint8_t li_vn_mode;     // Eight bits. li, vn, and mode.
                            	// li.   Two bits.   Leap indicator.
                            	// vn.   Three bits. Version number of the protocol.
@@ -38,7 +36,6 @@ typedef struct
 	uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
 	uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
 
-							 
 } ntp_packet;				 // summary 384 bytes
 
 
@@ -46,46 +43,39 @@ typedef struct
 #define NTP_TIMESTAMP_DELTA 2208988800ull
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char hostname[256];
 
-	if(argc < 2)
-	{
+	if(argc < 2) {
 		printf("required command-line argument <hostname> which is ntp-server\n");
 		exit(0);
-	}
-	else if(argc > 2)
-	{
+	} else if(argc > 2) {
 		printf("using only one command-line argument %s\n", argv[1]);
 	}
 	
-
-	if(strcpy(hostname, argv[1]) == NULL)
-	{
+	if(strcpy(hostname, argv[1]) == NULL) {
 		perror("error in copy argv[1] to hostname");
 	}
-	printf("hostname = %s\n", hostname);
-
+	printf("ntp-server = %s\n", hostname);
 
 	ntp_packet packet = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	memset(&packet, 0, sizeof(ntp_packet));
 
-	// Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
+	// Set the first byte's bits to 00,011,011
+	// for li = 0, vn = 3, and mode = 3. 
+	// The rest will be left set to zero.
 	*((char *) &packet + 0) = 0x1b;	// Represents 27 in base 10 or 00011011 in base 2.
 
 	struct sockaddr_in server_address;
 	struct hostent *server;
 
 	int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if(sockfd < 0)
-	{
+	if(sockfd < 0) {
 		perror("error in creating socket!");
 	}
 
 	server = gethostbyname(hostname);	// convert URL to IP-address
-	if(server == NULL)
-	{
+	if(server == NULL) {
 		perror("Host not finded");
 	}
 
@@ -101,23 +91,19 @@ int main(int argc, char *argv[])
 
 	int status;
 	status = connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address));
-	if(status < 0)
-	{
+	if(status < 0) {
 		perror("error with connecting");
 	}
 
-
 	int byte_count;
 	byte_count = write(sockfd, (char *) &packet, sizeof(ntp_packet));
-	if(byte_count < 0)
-	{
+	if(byte_count < 0) {
 		perror("error in writing in socket");
 	}
 
 	// reading a message
 	byte_count = read(sockfd, (char *) &packet, sizeof(ntp_packet));
-	if(byte_count < 0)
-	{
+	if(byte_count < 0) {
 		perror("error in reading from socket");
 	}
 
@@ -129,6 +115,6 @@ int main(int argc, char *argv[])
 
 	printf("Time: %s", ctime((const time_t *) &txTm));
 
-	printf("exit program\n");
+	// printf("exit program\n");
 	return 0;
 }
